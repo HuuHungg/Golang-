9f52275744930ac90e5ea505f4b670078c5a5e39
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-20 15:16:03 +0700

    add file modfile

--- index.go
+++ index.go
@@ -1,102 +1,74 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	// Boolean
-	myBoll := true
-	fmt.Println(myBoll)
+	// Cách sử dụng if else 
+	number := 10 
 
-	mySecond := false
-	fmt.Println(mySecond)
-	
-	// String
-	myString := "毎日ITを勉強してる"
-	fmt.Println(myString)
-	
-	//int kiểu số nguyên
-	myInt := 123
-	fmt.Println(myInt)
-	
-	//int 8,16,32,64 
-	// 1.Range
-	// 2. Bits
-	
-	//1. Range int8 (-128 - 127)
-	fmt.Println(math.MinInt8)
-	fmt.Println(math.MaxInt8)
+	// if condition {//code here}
+	if number == 10 {
+		fmt.Println("number = 10")
+	}
 
-	//1. Range int16 (-32768 - 32768)
-	fmt.Println(math.MinInt16)
-	fmt.Println(math.MaxInt16)
+	// if else
+	if number < 10 {
+		fmt.Println("number < 10")
+	}else {
+		fmt.Println("number = 10")
+	}
 
-	//1. Range int32 (-2147483648 - 2147483647)
-	fmt.Println(math.MinInt32)
-	fmt.Println(math.MaxInt32)
+	// if statement, condition {//code}
+	if a:= 100; a==100 {
+		fmt.Println("a = 100")
+	}
 
-	//1. Range int64 (-9223372036854775808 - 9223372036854775807)
-	fmt.Println(math.MinInt64)
-	fmt.Println(math.MaxInt64)
+	// switch case
+	number2 := 20
+	switch number2 {
+	case 1,200,300:
+		fmt.Println("number = 1")
+	case 2:
+		fmt.Println("number = 2")
+	case 3:
+		fmt.Println("number = 3")
+	case 20:
+		fmt.Println("number = 4")
+	default:
+		fmt.Println("unknown")
 
-	// // 2 Bits 
-	// fmt.Println("===========")
-	// fmt.Println(bits.OnesCount8(math.MaxUint8))
-	// fmt.Println(bits.OnesCount16(math.MaxUint16))
-	// fmt.Println(bits.OnesCount32(math.MaxUint32))
-	// fmt.Println(bits.OnesCount64(math.MaxUint64))
+	}
 
-	// =================================================
-	// uint	Số nguyên dương
+	number3 := 30
+	switch {
+	case number3 > 10:
+			fmt.Println("number > 10")
+	case number3 == 10:
+			fmt.Println("number == 10")
+	}
 
-	var myInt2 uint = 10  // uint là gán số nguyên dương nếu số này âm thì báo lỗi)
-	fmt.Println(myInt2)
-	
-	// =================================================
-	// Byte: bản chất của byte là unit8
-	var myByte byte = 255
-	fmt.Println(myByte)
-	// fmt.Println(math.MaxUint8)
-	fmt.Printf("T", myByte)
-	
-	// =================================================
-	var a byte = 'E'
-	fmt.Printf("%X", a)
+	// FallThrough Tiếp tục Math
+	number4 := 10
+	switch number4 {
+		case 1:
+			fmt.Println("number = 1")
+			fallthrough
+		case 10: 
+			if number == 10 {
+				goto handleNumberEqual10
+			}	
+			handleNumberEqual10:
+				fmt.Println("handle for case = 10")
+				break
+		case 2:
+			fmt.Println("number = 1")
+			fallthrough
+		case 3:
+			fmt.Println("number = 3")
+			fallthrough
+		case 4:
+			fmt.Println("number = 4")
+		
+	}
 
-	// ===================
-	fmt.Println("================")
-	// float Kiểu dữ liệu số thực
-	var myFloat float64 = 10.01
-	fmt.Println(myFloat)
-
-	// Complex Kiểu dữ liệu số phức z = a + bi (a là phần nguyên b là phần ảo)
-	var z complex64 = 10 + 1i
-	fmt.Println(z)
-
-	var z2 complex64 = 10 + 2i
-	fmt.Println(z + z2)
-	
-	// ===========================================================
-	// Rune
-	var myString2 string = "Nhẫn"
-
-	runes := []rune(myString2)
-
-	for i := 0; i < len(runes); i++ {
-		fmt.Printf("%c", runes[i])
-	}	 
-
-	var myRune rune = 'A'
-	fmt.Printf("%T", myRune)
-
-	
-	var c int = 1
-	var d float64 = 2.1
-	fmt.Println(float64(c) + d)
-
-	const PI = 3.14159
-	fmt.Println(PI)
-} 
-
+}
